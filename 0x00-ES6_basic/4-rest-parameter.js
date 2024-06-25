888be5c48f1e48e7fa02fe0f3bf9d42a16b1d4ee
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-06-25 16:12:30 +0300

    update

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,3 @@
 export default function returnHowManyArguments(...args) {
-  let argsLenght = []
-  for (const arg of args) {
-    argsLenght.push(arg)
-  }
-  return argsLenght.length
+  return args.length;
 }
