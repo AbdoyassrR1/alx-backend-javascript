e59eec73df74e683807d5c9ff0fafe1133920e7a
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-07-01 19:01:38 +0300

    updata

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,11 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
-  return studentList.filter((student) => student.location === city).map((student) => {
+  const filteredStudents = studentList.filter((student) => student.location === city);
+  const updateStudentGrade = filteredStudents.map((student) => {
     const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
     return {
       ...student,
       grade: gradeObj ? gradeObj.grade : 'N/A',
     };
   });
+  return updateStudentGrade;
 }
