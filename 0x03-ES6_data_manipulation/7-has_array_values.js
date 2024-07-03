364c78bd2ed8e03fa7b98828c8cd4063f4dc2d1d
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-07-03 03:29:35 +0300

    disable array-callback-return

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,10 +1,10 @@
 export default function hasValuesFromArray(set, list) {
+  /* eslint-disable array-callback-return */
   let verdict = true;
   list.map((x) => {
     if (!set.has(x)) {
       verdict = false;
     }
   });
-
   return verdict;
 }
