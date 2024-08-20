e74b4ab600a3d3a22b0f7ef8c887587598c2adb8
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-08-21 00:48:02 +0300

    update

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,14 @@
-const { createServer } = require('node:http');
+const http = require('http');
 
-const hostname = 'localhost';
+const host = '127.0.0.1';
 const port = 1245;
 
-const server = createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, host, () => {});
+
+module.exports = app;
