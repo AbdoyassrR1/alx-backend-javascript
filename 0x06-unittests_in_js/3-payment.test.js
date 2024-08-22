6fb4cc52a117e30f266cf70c6cdeaaf1fa02297e
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-08-22 21:50:52 +0300

    Spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  it('should call Utils.calculateNumber with "SUM", 100, 20', function() {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    spy.restore();
+  });
+});
